f0ee79df7f67f2f78221b03efc9438220e1eba4f
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-15 13:28:46 -0400

    created a <p> with text content and appended to main header content

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var main = document.querySelector("#main")
 var mainHeading = document.querySelector("#main-heading")
 var formEl = document.querySelector(".nameForm")
+var scorePassed = document.querySelector("#score-passed")
+var mainHeaderContent = document.querySelector("#main-header-content")
 
 //targeting highscore and storing scores in local storage in console//
 var score = localStorage.getItem("score")
@@ -13,3 +15,9 @@ var highScoresH1 = document.createElement("h1")
   highScoresH1.textContent = 'High Scores Chart!'
   highScoresH1.setAttribute('style', 'margin: auto; width: 50%; text-align: center;');
   mainHeading.appendChild(highScoresH1);
+
+  //created a <p> with text content and appended to main header content//
+  var highScoresP = document.createElement("p")
+  highScoresP.textContent = 'Enter your name to save new high score!'
+  highScoresP.setAttribute('style', 'margin: auto; width: 50%; text-align: center;');
+  mainHeaderContent.appendChild(highScoresP);
