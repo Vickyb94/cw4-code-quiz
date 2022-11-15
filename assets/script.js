d9bc765d941861aea8a64b95adf38df631e4181b
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-15 12:42:39 -0400

    created variable for highscore and to store scores in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+var main = document.querySelector("#main")
+var mainTitle = document.querySelector("#main-title")
+var formEl = document.querySelector(".nameForm")
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Code Quiz</title>
-  <link rel="stylesheet" href="./assets/css/style.css">
-</head>
+//targeting highscore and storing scores in local storage in console//
+var score = localStorage.getItem("score")
+var highscore = 0;
+var storedHighScore = localStorage.getItem("highscore")
 
-<body>
-  <header class="header" id="header">
-    <div class="button-container">
-      <a href="./high-scores.html">View high scores</a>
-    </div>
-      <h2 class="timer" id="timer"></h2>
-  </header>  
-
-  <main class="main" id="main">
-    <div id="main-title">
-    </div>
-
-
-
-
-    
-    <div id="above-main-content">
-    </div> 
-   
-   
-    <div id="main-content">
-      
-      
-    </div>
-    
-    <div id="main-sub-content">
-    </div>
-
-    <div id="main-button-section">
-    </div>
-
-    <div id="lower-section">
-    </div> 
-
-
-  </main>
-
-
-  <footer>
-    <p>
-      &copy; 2021 Daniel Urkov
-    </p>
-  </footer>
-
-  <script src="./assets/js/script.js"></script>
-</body>
-
-</html>
