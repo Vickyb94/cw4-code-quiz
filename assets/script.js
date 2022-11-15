e2073b906b537be377f1d59b25b6e03cfa1efdd8
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2022-11-15 14:10:29 -0400

    added the submit variable and styled the submit button in CSS

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@ var scorePassed = document.querySelector("#score-passed")
 var mainHeaderContent = document.querySelector("#main-header-content")
 var mainContent = document.querySelector("#main-content")
 var formEl = document.querySelector(".nameForm")
+var mainSubContent = document.querySelector("#main-sub-content")
+var mainButtonSection = document.querySelector("#main-button-section")
 
 //targeting highscore and storing scores in local storage in console//
 var score = localStorage.getItem("score")
@@ -14,7 +16,7 @@ var storedHighScore = localStorage.getItem("highscore")
 //text content for highscore//
 //adding highscore element to main heading//
 var highScoresH1 = document.createElement("h1")
-  highScoresH1.textContent = 'Finished!'
+  highScoresH1.textContent = 'Game Over!'
   highScoresH1.setAttribute('style', 'margin: auto; width: 50%; text-align: center;');
   mainHeading.appendChild(highScoresH1);
 
@@ -30,3 +32,15 @@ var highScoresH1 = document.createElement("h1")
   nameForm.setAttribute('style', 'margin: auto; width: 100%; text-align: center;');
   nameForm.setAttribute('placeholder', "Initials Here");
   mainContent.appendChild(nameForm);
+
+  // insert submit button
+  var submitButton = document.createElement("button")
+    submitButton.className = "submitButton"
+    submitButton.textContent = 'Submit'
+    submitButton.setAttribute('style', 'margin: auto; width: 15%; text-align: center; border-radius: 30px;');
+    submitButton.setAttribute('type', 'submit');
+    mainButtonSection.appendChild(submitButton);
+    //submitButton.addEventListener("submit", submitHSFunction);
+
+    //timer
+
